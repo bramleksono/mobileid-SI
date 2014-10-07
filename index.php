a1ab47c0a85ef13cde2cd192e5fc2bcba176bdba
adriandwiananto <bangateng@yahoo.com>
2014-10-07 23:57:43 +0700

    comply heroku format

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+if ($handle = opendir('./data/pid')) {
+
+    while (false !== ($entry = readdir($handle))) {
+
+        if ($entry != "." && $entry != "..") {
+
+            echo "$entry\n";
+        }
+    }
+
+    closedir($handle);
+}
+?>
